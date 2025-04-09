05644587599c45577fb010c27b8c78881635e9bc
priyanshu69-a <priyanshurana5002@gmail.com>
2025-04-09 14:27:15 +0530

    piano.js
    code for piano keys added

--- guitar.js
+++ guitar.js
@@ -1,32 +1,90 @@
-const keys = ['a', 's', 'd', 'f'];
+// detecting button press
+var NumberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-keys.forEach(key => {
-  document.querySelector(`.${key}`).addEventListener("click", () => {
-    playSound(key);
-    animateButton(key);
-  });
-});
+for(var i=0; i<NumberOfDrumButtons ;i++){
 
-document.addEventListener("keypress", (e) => {
-  if (keys.includes(e.key)) {
-    playSound(e.key);
-    animateButton(e.key);
-  }
-});
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var ButtonInnerHTML = this.innerHTML;
+        makesound(ButtonInnerHTML);
+        buttonAnimation(ButtonInnerHTML);
 
-function playSound(key) {
-  const soundMap = {
-    a: "sounds/guitar-1.mp3",
-    s: "sounds/guitar-2.mp3",
-    d: "sounds/guitar-3.mp3",
-    f: "sounds/guitar-4.mp3"
-  };
-  const audio = new Audio(soundMap[key]);
-  audio.play();
+        
+    });
 }
 
-function animateButton(key) {
-  const button = document.querySelector("." + key);
-  button.classList.add("pressed");
-  setTimeout(() => button.classList.remove("pressed"), 100);
+// detecting keyboard sound
+
+document.addEventListener("keypress", function(event){ 
+
+    makesound(event.key);
+    
+    buttonAnimation(event.key);
+});
+
+
+function makesound(key){
+
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/key17.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/key14.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/key21.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/key22.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/key17.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/key18.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/key19.mp3");
+            audio.play();
+            break;
+        case "m":
+            var audio = new Audio("sounds/key01.mp3");
+            audio.play();
+            break;  
+        case "n":
+            var audio = new Audio("sounds/key04.mp3");
+            audio.play();
+            break;
+        case "o":
+            var audio = new Audio("sounds/key07.mp3");
+            audio.play();
+            break;             
+        default:
+            break;
+    }
 }
+function buttonAnimation(currentkey) {
+
+    var activeButton = document.querySelector("."+ currentkey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function()  {
+        activeButton.classList.remove("pressed");
+    } ,100);
+}
+
+
+
+
+
+
+
+
+
