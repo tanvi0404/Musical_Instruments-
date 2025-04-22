c6d7bfec1f93209810ccd1a7ccf99d72d0eac6ef
rijul19 <rijul0688.becse24@chitkara.edu.in>
2025-04-22 14:58:25 +0530

    Update bongo.js

--- bongo.js
+++ bongo.js
@@ -11,3 +11,22 @@ for(var i=0; i<NumberOfDrumButtons ;i++){
         
     });
 } 
+function makesound(key){
+
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/BngHghOpnQt_SP_223_02.wav");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/Bongo_05_20_SP.wav");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/BongoLowOpenQuiet_SP_223_01.wav");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
